db66a43e8c3a58a0ce045765ef42065124a5f42c
Carson Keller <carsonjkeller@gmail.com>
2020-03-25 11:53:01 -0600

    Initial Commit - FINAL

---
+++ fizzBuzz - do-while.js
@@ -0,0 +1,9 @@
+i = 1
+
+do {
+    if (i % 3 == 0 && i % 5 == 0) {console.log('FizzBuzz');}
+    else if (i % 5 == 0) {console.log('Buzz');}
+    else if (i % 3 == 0) {console.log('Fizz');}
+    else {console.log(i);}
+    i++;
+} while (i <= 100)
